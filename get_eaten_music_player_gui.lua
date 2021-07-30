c69be5ba1df9c8fdb8bdce5c6158a03f7348ca1b
MiRw3b <60517683+MiRw3b@users.noreply.github.com>
2021-07-30 23:46:58 +0200

    Add files via upload
    one of my script. -c00lkidd

---
+++ get_eaten_music_player_gui.lua
@@ -0,0 +1,6 @@
+--READ BEFORE USING!
+--You need to be in a car for the songs to play!
+--Sometimes there is a bug so expect to have to click 2, 3 or even 4 times on the play button for the song to play!
+--To stop the song, you have to push the vehicule in the void.
+
+loadstring(game:GetObjects("rbxassetid://6818350301")[1].Source)()
